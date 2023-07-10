2e653f97ca564f23a91fd4389f4653f5b14b1b77
bazayd <brandonzarriaga@gmail.com>
2023-07-10 00:24:41 -0700

    Main Fix

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static int SORT_MAX_SIZE = 16;
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
