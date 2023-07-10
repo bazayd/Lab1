5559ec32cff4c7b4202b9080ce0f37cb95cdb8f0
Brandon Arriaga <brandonzarriaga@gmail.com>
2023-07-09 21:19:14 -0700

    First Commit.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
-
+    // Default Array size
     public static int SORT_MAX_SIZE = 16;
     public static void main(String[] args) {
 
